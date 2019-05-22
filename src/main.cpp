331a7a05c98edea1f9b3fb9ae8f27ccade6f17d7
Hank Barta <hbarta@gmail.com>
2019-05-22 11:21:14 -0500

    allocate and populate test data buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char** argv) 
 {
     std::cout << "Hello World" << std::endl;
+
+    int dataBlockSize       = 1024;
+    int* buffer = new int[dataBlockSize];
+    std::cout <<"sizeof int = " << sizeof buffer[0] << std::endl;
+    std::cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << std::endl;
+    std::cout <<"max random number = " << RAND_MAX << std::endl;
+    srand(time(NULL));
+    for(int i=0; i<dataBlockSize; i++)
+        buffer[i] = rand();
+
+    for(int i=0; i<10; i++)
+        std::cout << buffer[i] << " ";
+    std::cout << std::endl;
+
+    delete[] buffer;
 }
