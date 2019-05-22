439a3c8cce1ea8291c0bd3207e55253a2771a8ac
Hank Barta <hbarta@gmail.com>
2019-05-22 11:27:55 -0500

    add 'using namespace std'

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 int main(int argc, char** argv) 
 {
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << endl;
 
     int dataBlockSize       = 1024;
     int* buffer = new int[dataBlockSize];
-    std::cout <<"sizeof int = " << sizeof buffer[0] << std::endl;
-    std::cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << std::endl;
-    std::cout <<"max random number = " << RAND_MAX << std::endl;
+    cout <<"sizeof int = " << sizeof buffer[0] << endl;
+    cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << endl;
+    cout <<"max random number = " << RAND_MAX << endl;
     srand(time(NULL));
     for(int i=0; i<dataBlockSize; i++)
         buffer[i] = rand();
 
     for(int i=0; i<10; i++)
-        std::cout << buffer[i] << " ";
-    std::cout << std::endl;
+        cout << buffer[i] << " ";
+    cout << endl;
 
     delete[] buffer;
 }
