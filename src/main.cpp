b2cbdc66c690837b9ce3343614b12f160f210808
Hank Barta <hbarta@gmail.com>
2019-05-22 13:18:02 -0500

    write buffer to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 using namespace std;
@@ -8,6 +9,9 @@ int main(int argc, char** argv)
     cout << "Hello World" << endl;
 
     int dataBlockSize       = 1024;
+    int writeCount = 256*1024;
+    string filename = "test.dat";
+
     int* buffer = new int[dataBlockSize];
     cout <<"sizeof int = " << sizeof buffer[0] << endl;
     cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << endl;
@@ -20,5 +24,12 @@ int main(int argc, char** argv)
         cout << buffer[i] << " ";
     cout << endl;
 
+    ofstream        testFile(filename);
+    //testFile.open(filename);
+    for(int i=0; i<writeCount; i++)
+        testFile.write((const char*)buffer, sizeof buffer[0]*dataBlockSize);
+    testFile.close();
+
     delete[] buffer;
+    return 0;
 }
