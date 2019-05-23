365c6cf8bb60dc976242bc09fb35dfe3793b177e
Hank Barta <hbarta@gmail.com>
2019-05-22 21:40:58 -0500

    coalesce time marks (and apparent reformatting.)

--- src/main.cpp
+++ src/main.cpp
@@ -5,47 +5,52 @@
 
 using namespace std;
 
-int main(int argc, char** argv) 
+int main(int argc, char **argv)
 {
     cout << "Hello World" << endl;
 
-    int dataBlockSize       = 1024;     // size of each write (in ints)
-    int writeCount = 256*1024;          // times to write the buffer
+    int dataBlockSize = 1024;    // size of each write (in ints)
+    int writeCount = 256 * 1024; // times to write the buffer
     string filename = "test.dat";
 
-    clock_t     previous;               // used to measure clock() intervals
-    clock_t     now;                    // for each write
+    clock_t previous; // used to measure clock() intervals
+    clock_t now;      // for each write
 
+    int *buffer = new int[dataBlockSize];         // data buffer to write
+    clock_t *timeMarks = new clock_t[writeCount]; // buffer to store time marks
 
-    int* buffer = new int[dataBlockSize];   // data buffer to write
-    clock_t* timeMarks = new clock_t[writeCount];   // buffer to store time marks
-
-    cout <<"sizeof int = " << sizeof buffer[0] << endl;
-    cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << endl;
-    cout <<"max random number = " << RAND_MAX << endl;
-    cout <<"CLOCKS_PER_SEC = " << CLOCKS_PER_SEC << endl;
+    cout << "sizeof int = " << sizeof buffer[0] << endl;
+    cout << "sizeof buffer = " << sizeof buffer[0] * dataBlockSize << endl;
+    cout << "max random number = " << RAND_MAX << endl;
+    cout << "CLOCKS_PER_SEC = " << CLOCKS_PER_SEC << endl;
 
     // populate buffer with random data
     srand(time(NULL));
-    for(int i=0; i<dataBlockSize; i++)
+    for (int i = 0; i < dataBlockSize; i++)
         buffer[i] = rand();
 
     // write the data
-    ofstream        testFile(filename);
+    ofstream testFile(filename);
     previous = clock();
-    for(int i=0; i<writeCount; i++) {
-        testFile.write((const char*)buffer, sizeof buffer[0]*dataBlockSize);
+    for (int i = 0; i < writeCount; i++)
+    {
+        testFile.write((const char *)buffer, sizeof buffer[0] * dataBlockSize);
         now = clock();
         timeMarks[i] = now - previous;
         previous = now;
     }
     testFile.close();
 
-    // report the results
-    for(int i=0; i<writeCount; i++) {
-        cout << timeMarks[i] << endl;
-    }
+    int coalesce = 200; // coalesce readings to report
 
+    // report the results
+    for (int i = 0; i < writeCount / coalesce; i++)
+    {
+        int accum = 0;
+        for (int j = 0; j < coalesce; j++)
+            accum += timeMarks[i * coalesce + j];
+        cout << accum << endl;
+    }
 
     delete[] buffer;
     delete[] timeMarks;
