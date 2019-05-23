c70e6ec429b065559d4e9e41275bf801850310be
Hank Barta <hbarta@gmail.com>
2019-05-22 21:18:54 -0500

    collect/report time marks + commentary

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -8,28 +9,45 @@ int main(int argc, char** argv)
 {
     cout << "Hello World" << endl;
 
-    int dataBlockSize       = 1024;
-    int writeCount = 256*1024;
+    int dataBlockSize       = 1024;     // size of each write (in ints)
+    int writeCount = 256*1024;          // times to write the buffer
     string filename = "test.dat";
 
-    int* buffer = new int[dataBlockSize];
+    clock_t     previous;               // used to measure clock() intervals
+    clock_t     now;                    // for each write
+
+
+    int* buffer = new int[dataBlockSize];   // data buffer to write
+    clock_t* timeMarks = new clock_t[writeCount];   // buffer to store time marks
+
     cout <<"sizeof int = " << sizeof buffer[0] << endl;
     cout <<"sizeof buffer = " << sizeof buffer[0]*dataBlockSize << endl;
     cout <<"max random number = " << RAND_MAX << endl;
+    cout <<"CLOCKS_PER_SEC = " << CLOCKS_PER_SEC << endl;
+
+    // populate buffer with random data
     srand(time(NULL));
     for(int i=0; i<dataBlockSize; i++)
         buffer[i] = rand();
 
-    for(int i=0; i<10; i++)
-        cout << buffer[i] << " ";
-    cout << endl;
-
+    // write the data
     ofstream        testFile(filename);
-    //testFile.open(filename);
-    for(int i=0; i<writeCount; i++)
+    previous = clock();
+    for(int i=0; i<writeCount; i++) {
         testFile.write((const char*)buffer, sizeof buffer[0]*dataBlockSize);
+        now = clock();
+        timeMarks[i] = now - previous;
+        previous = now;
+    }
     testFile.close();
 
+    // report the results
+    for(int i=0; i<writeCount; i++) {
+        cout << timeMarks[i] << endl;
+    }
+
+
     delete[] buffer;
+    delete[] timeMarks;
     return 0;
 }
